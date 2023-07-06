0fb72dfb564e83443d87207af35313d28d63b26c
Светлана <138018102+syakovleva@users.noreply.github.com>
2023-07-06 15:46:06 +0300

    Update RandomNumber.java
    Исправление угадываемого числа

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -6,7 +6,7 @@ public class RandomNumber {
         Random random = new Random();
         int randomNum = random.nextInt(100);
         System.out.print("Загаданное число: " + randomNum + "\n");
-        for (int guessesMade = 5; guessesMade < 100; guessesMade++) {
+        for (int guessesMade = 0; guessesMade < 100; guessesMade++) {
             if (randomNum < guessesMade) {
                 System.out.println("Число " + guessesMade + " больше того, что загадал компьютер");
             } else if (randomNum > guessesMade) {
